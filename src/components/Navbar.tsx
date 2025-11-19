0e539fa34205c779d0624198922ab85fd7a3e2a9
Mariah Tuquero <mariah27@hawaii.edu>
2025-11-19 10:58:24 -1000

    Match up page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,6 +28,9 @@ const NavBar: React.FC = () => {
                   <Nav.Link id="list-stuff-nav" href="/list" key="list" active={pathName === '/list'}>
                     List Stuff
                   </Nav.Link>,
+                  <Nav.Link id="match-nav" href="/match" key="match" active={pathName === '/match'}>
+                    Match
+                  </Nav.Link>,
                 ]
               : ''}
             {currentUser && role === 'ADMIN' ? (
