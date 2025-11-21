8f27da099102e2d9da16573e48b73545bb385b42
Alfred Agbayani <alfredmaric@gmail.com>
2025-11-20 20:01:34 -1000

    Done

---
+++ src/app/event/page.tsx
@@ -0,0 +1,11 @@
+import CreateEventForm from '@/components/CreateEventForm';
+
+const EventPage = () => {
+  return (
+    <main>
+      <CreateEventForm />
+    </main>
+  );
+};
+
+export default EventPage;
---
+++ src/app/events/page.tsx
@@ -0,0 +1,11 @@
+import CreateEventForm from '@/components/CreateEventForm';
+
+const Events = () => {
+  return (
+    <main>
+      <CreateEventForm />
+    </main>
+  );
+};
+
+export default Events;
